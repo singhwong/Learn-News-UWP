751e3ed0e4bb4cad072ff1e658f51ed4a317bc2d
singhwong <singhwongwxg@hotmail.com>
2019-01-11 18:06:11 +0800

    update sth

--- Views/NewsPage.xaml.cs
+++ Views/NewsPage.xaml.cs
@@ -35,7 +35,14 @@ namespace The_Paper.Views
         private async void TabView_TabSwitch(object sender, EventArgs e)
         {
             scrollViewer.ChangeView(null, 0, null);
-            await newsPageVM.LoadTab((e as TabSwitchEventArgs).tabIndex);
+            try
+            {
+                await newsPageVM.LoadTab((e as TabSwitchEventArgs).tabIndex);
+            }
+            catch
+            {
+            }
+           
             #region 优化点击顶部标题烂选项，右边会出现 NewsDetail Page bug,尚不知为何会出现这种情况
             colume_2.Width = new GridLength(0);
             back_button.Visibility = Visibility.Collapsed;
